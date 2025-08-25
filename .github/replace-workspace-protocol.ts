af8729eaff04efcd25300267926604054b7a45d4
ChefBingbong <133646395+ChefBingbong@users.noreply.github.com>
2025-08-25 01:55:15 +0100

    feat: patch-version-script (#80)
    * feat: patch version script
    
    * x
    
    * a

--- .github/replace-workspace-protocol.ts
+++ .github/replace-workspace-protocol.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'node:url'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const tracerPkgPath = join(__dirname, '..', 'packages', 'tracer', 'package.json')
 const utilsPkgPath = join(__dirname, '..', 'packages', 'utils', 'package.json')
+const cliPkgPath = join(__dirname, '..', 'packages', 'cli', 'package.json')
 
 type Pkg = {
   name: string
@@ -19,12 +20,13 @@ const load = (p: string) => JSON.parse(readFileSync(p, 'utf8')) as Pkg
 
 const tracer = load(tracerPkgPath)
 const utils = load(utilsPkgPath)
+const cli = load(cliPkgPath)
 
 const replaceWorkspaces = (obj?: Record<string, string>) => {
   if (!obj) return
   for (const [dep, range] of Object.entries(obj)) {
     if (!range?.startsWith('workspace:')) continue
-    if (dep === utils.name) {
+    if (dep === utils.name || dep === tracer.name) {
       const prefix = range.split(':')[1] === '*' ? '' : range.split(':')[1] // "workspace:^" -> "^"
       obj[dep] = `${prefix || ''}${utils.version}`
     }
@@ -37,4 +39,7 @@ replaceWorkspaces(tracer.peerDependencies)
 replaceWorkspaces(tracer.optionalDependencies)
 
 writeFileSync(tracerPkgPath, `${JSON.stringify(tracer, null, 2)}\n`)
+writeFileSync(cliPkgPath, `${JSON.stringify(cli, null, 2)}\n`)
+
 console.log(`Replaced workspace deps in ${tracerPkgPath}`)
+console.log(`Replaced workspace deps in ${cliPkgPath}`)
