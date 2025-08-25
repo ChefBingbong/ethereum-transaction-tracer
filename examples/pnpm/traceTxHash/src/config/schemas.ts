f421fb08f2f43c0eb7e42ef957f258c87d14a58e
Evan McGrane <evanmcgranebb98@gmail.com>
2025-08-25 10:54:48 +0100

    feat: add runtime examples

---
+++ examples/pnpm/traceTxHash/src/config/schemas.ts
@@ -0,0 +1,14 @@
+import type { Hex } from 'viem'
+import { z } from 'zod'
+
+export type NodeEnv = z.infer<typeof zNodeEnv>
+
+export const zNodeEnv = z.enum(['production', 'development', 'local'])
+
+export const zPort = (defaultPort = 80) => z.coerce.number().default(defaultPort)
+
+export const zEVMPrivateKey = z
+  .string()
+  .regex(/^(0x)?[a-fA-F0-9]{64}$/)
+  .transform((val) => (val.startsWith('0x') ? (val as Hex) : (`0x${val}` as Hex)))
+  .refine((val) => val.length === 66 && val.startsWith('0x'), 'Invalid Ethereum private key format')
