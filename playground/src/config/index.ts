8d646db7ae6f95792c76c96ab25b71e28c9cd717
ChefBingbong <133646395+ChefBingbong@users.noreply.github.com>
2025-08-29 14:41:17 +0100

    feat: add-playground-workspace-for-local-dev (#113)
    * feat: add playground workspace
    
    * chore: comit extra
    
    * chore: rm test script in playground
    
    * chore: save

---
+++ playground/src/config/index.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+import { zNodeEnv } from './schemas'
+
+export const baseEnvVars = z.object({
+  NODE_ENV: zNodeEnv.default('development'),
+})
+
+export function getEnvVarsWithBase<T extends z.ZodRawShape>(additional: T) {
+  const mergedSchema = baseEnvVars.extend(additional)
+  return mergedSchema.parse(process.env)
+}
+
+export const { ETHERSCAN_API_KEY } = getEnvVarsWithBase({
+  ETHERSCAN_API_KEY: z.string().optional(),
+})
