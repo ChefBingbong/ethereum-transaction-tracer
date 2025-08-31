75f92a5a12dd5afd6ee3f9819c142c113b5a843e
ChefBingbong <133646395+ChefBingbong@users.noreply.github.com>
2025-08-31 19:19:13 +0100

    fix: cli-err and prepare script (#125)
    * chore: fix cli
    
    * chore: add prepare script
    
    * chore: bump
    
    * chore: build
    
    * c

--- packages/cli/src/tasks/convertions/to-utf8.ts
+++ packages/cli/src/tasks/convertions/to-utf8.ts
@@ -5,7 +5,7 @@ import { parseHex } from './to-asci'
 
 createTask('to-utf8')
   .description('Convert hex data to a utf-8 string')
-  .aliases(['thd', '2hd'])
+  .aliases(['tu8', '2u8'])
   .argument('<hexdata>', 'Hex data (0x-optional)', parseHex)
   .action((hexData) => {
     logger.default(hexToString(normalizeHexData(hexData)))
