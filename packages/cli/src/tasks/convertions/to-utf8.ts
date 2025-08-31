4aaca718a2d99a0be584581ccd2efb0028f75ff5
ChefBingbong <133646395+ChefBingbong@users.noreply.github.com>
2025-08-31 18:58:59 +0100

    chore: fix cli duplicate export (#123)

--- packages/cli/src/tasks/convertions/to-utf8.ts
+++ packages/cli/src/tasks/convertions/to-utf8.ts
@@ -3,8 +3,8 @@ import { hexToString } from 'viem/utils'
 import createTask from '../../program'
 import { parseHex } from './to-asci'
 
-createTask('to-hexdata')
-  .description('Normalize input to lowercase, 0x-prefixed hex')
+createTask('to-utf8')
+  .description('Convert hex data to a utf-8 string')
   .aliases(['thd', '2hd'])
   .argument('<hexdata>', 'Hex data (0x-optional)', parseHex)
   .action((hexData) => {
